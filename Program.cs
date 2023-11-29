60c8fc19debb4f3f8e75fa353224b95ece265d58
ferh0162 <ferh0162@stud.kea.dk>
2023-11-29 14:00:36 +0100

    exercise 2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,16 @@ public class TuplesExercise
 {
     public static void Main(){
         var TuplesExercise = new TuplesExercise();
-        TuplesExercise.Exercise1();
+        //TuplesExercise.Exercise1();
+        var Exercise2test = TuplesExercise.Exercise2("Ferhat");
+        Console.WriteLine(Exercise2test);
+
+        var ex2item1 = Exercise2test.Item1;
+        var ex2item2 = Exercise2test.Item2;
+
+        Console.WriteLine("item1: " + ex2item1);
+        Console.WriteLine("item2: " + ex2item2);
+
     }
 
     public void Exercise1()
@@ -20,13 +29,23 @@ public class TuplesExercise
 
         Console.WriteLine(tuple1);
         Console.WriteLine(tuple1.Item1);
+
         Console.WriteLine(tuple2);
         Console.WriteLine(tuple2.Item1);
+
         Console.WriteLine(tuple3);
         Console.WriteLine(tuple3.Item1);
         Console.WriteLine(tuple3.Boolean);
 
         
 
+    }
+
+    public (int, string) Exercise2(string name)
+    {
+        int length = name.Length;
+        string uppercasedName = name.ToUpper();
+
+        return (length, uppercasedName);
     }
 }
